49c976a96764bc62a462abdae4f45e1a903439df
siddardha kolanupaka <siddardhakolanupaka@siddardhas-mbp.home>
2019-09-07 22:35:04 +1000

    Drag and Drop

--- src/test/java/AppiumTestingFramework/Drag_Drop.java
+++ src/test/java/AppiumTestingFramework/Drag_Drop.java
@@ -2,8 +2,14 @@ package AppiumTestingFramework;
 
 import java.net.MalformedURLException;
 
+import org.openqa.selenium.WebElement;
+
+import io.appium.java_client.TouchAction;
 import io.appium.java_client.android.AndroidDriver;
+import static io.appium.java_client.touch.offset.ElementOption.element;
 import io.appium.java_client.android.AndroidElement;
+import static io.appium.java_client.touch.LongPressOptions.longPressOptions;
+
 
 public class Drag_Drop extends Base {
 
@@ -13,8 +19,14 @@ public class Drag_Drop extends Base {
 		AndroidDriver<AndroidElement> driver=Capabilities();
 		driver.findElementByAndroidUIAutomator("text(\"Views\")").click();
 		driver.findElementByAndroidUIAutomator("text(\"Drag and Drop\")").click();
+		TouchAction action =new TouchAction(driver);
+		WebElement source=driver.findElementsByClassName("android.view.View").get(0);
+		WebElement dest =driver.findElementsByClassName("android.view.View").get(2);
+		action.longPress(longPressOptions().withElement(element(source))).moveTo(element(dest)).release().perform();
 
+		// action.longPress(element(source)).moveTo(element(dest)).release().perform();
 		
+		// if duration not needed above orelse used in code..
 
 
 
