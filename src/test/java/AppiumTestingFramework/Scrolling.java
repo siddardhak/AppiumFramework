697b0d2fd194375de6b91030a8da87f86946a091
siddardha kolanupaka <siddardhakolanupaka@siddardhas-mbp.home>
2019-09-07 22:35:57 +1000

    Scrolling

--- src/test/java/AppiumTestingFramework/Scrolling.java
+++ src/test/java/AppiumTestingFramework/Scrolling.java
@@ -2,6 +2,7 @@ package AppiumTestingFramework;
 
 import java.net.MalformedURLException;
 
+import io.appium.java_client.TouchAction;
 import io.appium.java_client.android.AndroidDriver;
 import io.appium.java_client.android.AndroidElement;
 
@@ -12,7 +13,9 @@ public class Scrolling extends Base{
 		AndroidDriver<AndroidElement> driver=Capabilities();
 		
 		driver.findElementByAndroidUIAutomator("text(\"Views\")").click();
-		driver.findElementByAndroidUIAutomator("new UiScrollable(new UiSelector()).scrollIntoView(text(\"WebView\"));").click();;
+		driver.findElementByAndroidUIAutomator("new UiScrollable(new UiSelector()).scrollIntoView(text(\"WebView\"));").click();
+		
+		
 
 	}
 
